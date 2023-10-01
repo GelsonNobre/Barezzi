325f8ca7f04064ded50a023c4016bdd098687071
GelsonNobre <gelsonnobre2731@gmail.com>
2023-10-01 16:37:32 -0300

    Inicio do Projeto
    Criação das primeiras classes e telas

---
+++ Barezzi/src/main/java/com/mycompany/barezzi/Barezzi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.barezzi;
+
+/**
+ *
+ * @author gelso
+ */
+public class Barezzi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
